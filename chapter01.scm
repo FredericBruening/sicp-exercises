4ddea28b79a0ec7ce7f8c574fe57c4e292220334
FredericBruening <contact@fredesk.com>
2023-10-20 12:10:19 -0500

    chapter01-exercises_6

--- chapter01.scm
+++ chapter01.scm
@@ -1,10 +1,10 @@
-;; exercise 1.1
+;; Exercise 1.1
 ;; this exercise was actually to interpret the code yourself, so there is no sense in adding it here
 
-;; exercise 1.2
+;; Exercise 1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))) (* 3 (- 6 2) (- 2 7)))))
 
-;; exercice 1.3
+;; Exercice 1.3
 (define (sumsquare a b c) (cond ((and (< a b) (< a c)) (+ (* b b) (* c c)))
 			     ((and (< b a) (< b c)) (+ (* a a) (* c c)))
 			     (else (+ (* a a) (* b b))))
@@ -12,7 +12,7 @@
 
 (sumsquare 1 2 3)
 
-;; exercise 1.4
+;; Exercise 1.4
 ;;(define (a-plus-abs-b a b)
 ;;  ((if (> b 0) + -) a b))
 ;;
@@ -20,7 +20,7 @@
 ;; > b 0 is evaluated first, then the conditional operator if, which results in a compound operator + or -
 ;; the compound operator with the arguments will be evaluated in the end
 
-;; exercise 1.5
+;; Exercise 1.5
 ;; given
 ;; (define (p) (p))
 ;; (define (test x y) (if (= x 0) 0 y))
@@ -39,3 +39,14 @@
 (if (= 0 0) 0 (p))
 ; since (p) is a procedure there nothing to evaluate there, cannot be converted to primitive type
 ; result is 0
+
+;; Exercise 1.6
+;; having defined new-if
+;; since the interpreter uses the applicative-order evaluation. arguments are evaluated before the procedure
+;; this would mean that if we replace the special conditional expression with a procedure, the arguments will be evaluated first
+;; and then applied to the procedure which would make the program inefficient
+;; the result stays the same though
+
+;; Exercise 1.7
+;;
+todo
